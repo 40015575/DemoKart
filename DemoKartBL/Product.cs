e2b232e24a8fc919653c2dfd102c9e009814a4fa
99003552 <78849738+99003552@users.noreply.github.com>
2021-02-25 23:38:05 +0530

    Update Product.cs

--- DemoKartBL/Product.cs
+++ DemoKartBL/Product.cs
@@ -8,25 +8,23 @@ namespace DemoKartBL
 {
     public class Product
     {
-      
-        public static Dictionary<String, int> product = new Dictionary<String, int>(){
-            {"PID200",150},{"PID300",10 },{"PID60",20 },{"PID10",15 },{"PID13",100}, };
-        public static Dictionary<String, int> quantityPid = new Dictionary<String, int>(){
-                {"PID200", 10 },{"PID300",2 },{"PID60",1 },{"PID10",0 },{"PID13",5}, };
-        public static Dictionary<String, String> Proname = new Dictionary<String, String>(){
-                {"PID200", "laptop", },{"PID300", "Cashew"},{"PID60","BodySpray"},{"PID10","Mobile" },{"PID13","Charger"}, };
-         public void display()
+       public int ProductId { get; set; }
+        public String ProductName { get; set; }
+        public int ProductQuantity { get; set; }
+        public double ProductPrice { get; set; }       
+        public Category CObj = new Category();     
+        public Product()
         {
-            Console.WriteLine("list of product id and prices\n 1. product id    2.price");
-            foreach (KeyValuePair<String, int> item in product)
-            {
-                Console.WriteLine(item.Key + "\t" + item.Value);
-            }
-            Console.WriteLine("list of product id and quantitites\n 1. product id    2.quantity"); ;
-            foreach (KeyValuePair<String, int> item in quantityPid)
-            {
-                Console.WriteLine(item.Key + "\t" + item.Value);
-            }
         }
+        public Product(int productId, String productName, double price, int quantity,int CategoriesId,String Categories)
+        {
+            this.ProductId = productId;
+            this.ProductName = productName;
+            this.ProductQuantity = quantity;
+            this.ProductPrice = price;
+            CObj.CategoriesId = CategoriesId;
+            CObj.CategoryName = Categories;
+        }
+        
     }
 }
