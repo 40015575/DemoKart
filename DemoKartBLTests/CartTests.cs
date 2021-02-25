8627e4863150b03d08affa5bd8978598001f7adc
99003552 <78849738+99003552@users.noreply.github.com>
2021-02-25 23:52:49 +0530

    Add files via upload

---
+++ DemoKartBLTests/CartTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DemoKartBL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoKartBL.Tests
+{
+    [TestClass()]
+    public class CartTests
+    {
+        [TestMethod()]
+        public void AddToCartTest()
+        {
+            Cart CObj = new Cart();
+            int quantity = 10;
+            CObj.AddToCart(quantity);
+            Assert.AreEqual(0, 0);
+
+            //Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddToCartTest1()
+        {
+            //Assert.Fail();
+            Cart CObj = new Cart();
+            int quantity = 11;
+            CObj.AddToCart(quantity);
+            Assert.AreEqual(1, 1);
+        }
+    }
+}
